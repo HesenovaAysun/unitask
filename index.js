5d245dd8a276bd75e30dfb0d4b132d5a5f8b35e4
Deaglesso <seid.beam@gmail.com>
2023-10-19 22:40:11 -0800

    added

---
+++ index.js
@@ -0,0 +1,73 @@
+
+//28
+// function newArray(arr){
+//   let array=[];
+//   for (let i = 0; i< arr.length; i++) {
+//     array.push(arr[i]**2);
+//   }
+//   return array;
+// }
+// let num=[1,2,3];
+// let numbers=newArray(num);
+// console.log(numbers);
+
+
+//29?
+// function  musbetsum(arr){
+//   let sum=0;
+//   for (let i = 0; i < arr.length; i++) {
+//    if(arr[i]>0){
+// sum+=arr[i];
+//    }
+//     if(arr[i]<0){
+//       return 0;
+//     }
+//     else{
+//       return  sum;
+//     }
+//   }
+// }
+// let numbers=[1,-2,4,-2];
+// let musbetsum=newmusbetsum(numbers);
+// console.log(musbetsum);
+
+//29
+// let arr=[2,3,5];
+// let sum=0;
+// for (let i = 0; i < arr.length; i++) {
+//  if (arr[i]>0) {
+//   sum+=arr[i];
+//  }
+//   else{
+//    return 0;
+//   } 
+// }
+// console.log (sum);
+
+//30
+// let arr=[1,2,3,4];
+// let max=0;
+// let  min=0;
+// let sum=0;
+
+// for (let i = 0; i < arr.length; i++) {
+//   if (min>arr[i]) {
+//     min=arr[i];
+//   }
+//   if (arr[i]>max){
+//     max=arr[i];
+//   }
+//   sum+=arr[i];
+//   average=sum/arr.length
+// media=(min+max)/2
+// }
+// console.log(media);
+// console.log(average);
+
+
+//35
+// const input=[["a","b","c"],["c","d","f"],["d","f","g"]];
+// const arr=input.flat();
+// console.log(input.flat());
+
+
